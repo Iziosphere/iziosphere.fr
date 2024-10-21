2a8331540d99e94e311430469bab46eb6edae9df
Grégoire DURAND <59779735+gregoire-drd@users.noreply.github.com>
2024-10-21 19:15:23 +0200

    feat : Register front page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,27 +2,33 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from "./pages/home/home.component";
 import { NotFoundComponent } from "./shared/not-found/not-found.component";
 import { LoginComponent } from './pages/login/login.component';
-import {IplocationComponent} from './pages/tools/iplocation/iplocation.component';
-import {TextEditorComponent} from './pages/tools/text-editor/text-editor.component';
+import { IplocationComponent } from './pages/tools/iplocation/iplocation.component';
+import { TextEditorComponent } from './pages/tools/text-editor/text-editor.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
 
 
-    {
-        path: '',
-        component: HomeComponent,
-        data: {
-            title: 'Home'
-        }
-    },
-    {
-        path: 'login',
-        component: LoginComponent,
-        data: {
-            title: 'Login'
-        }
-        // canActivate: [DisconnectedGuard]
-    },
+  {
+    path: '',
+    component: HomeComponent,
+    data: {
+      title: 'Home'
+    }
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    data: {
+      title: 'Login'
+    }
+    // canActivate: [DisconnectedGuard]
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    // canActivate: [DisconnectedGuard]
+  },
   {
     path: 'tools/ip-location',
     component: IplocationComponent,
@@ -36,16 +42,12 @@ export const routes: Routes = [
     data: {
       title: 'text editor'
     }
-  },    // {
-    //     path: 'register',
-    //     component: RegisterComponent,
-    //     canActivate: [DisconnectedGuard]
-    // },
-    {
-        path: '**',
-        component: NotFoundComponent,
-        data: {
-            title: 'Not Found'
-        }
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+    data: {
+      title: 'Not Found'
     }
+  }
 ];
