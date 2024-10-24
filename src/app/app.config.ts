337ede8d8274d3b956c3c3ff594b652c90a11ea4
julien <julien.sanchez74@orange.fr>
2024-10-24 23:54:53 +0200

    fix : scrollPositionRestoration on top + anchorScrolling if we back on the previous page

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,27 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter, withInMemoryScrolling, withRouterConfig} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import {provideToastr} from 'ngx-toastr';
 import {provideAnimations} from '@angular/platform-browser/animations';
 import {authInterceptor} from './interceptors/auth-interceptor';
 
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({eventCoalescing: true}), provideRouter(routes), provideHttpClient(withInterceptors([authInterceptor])), provideAnimations(), // required animations providers
+  providers: [provideZoneChangeDetection({eventCoalescing: true}), provideRouter(routes, withInMemoryScrolling({
+    scrollPositionRestoration: 'top',
+    anchorScrolling: 'disabled'
+  }), withRouterConfig({
+    paramsInheritanceStrategy: 'always',
+    onSameUrlNavigation: 'reload'
+  }),), provideHttpClient(withInterceptors([authInterceptor])),
+    provideAnimations(),
+    // required animations providers
     provideToastr({
       timeOut: 10000,
       positionClass: 'toast-bottom-right',
       preventDuplicates: true,
-    }),]
+    }),
+  ]
 };
