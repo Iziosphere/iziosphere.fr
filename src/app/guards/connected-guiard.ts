a16550f36de897f028f60917d1e429037b0294ec
Grégoire DURAND <59779735+gregoire-drd@users.noreply.github.com>
2024-10-26 18:58:06 +0200

    fix : Change messages on guards

--- src/app/guards/connected-guiard.ts
+++ src/app/guards/connected-guiard.ts
@@ -1,12 +1,13 @@
 import {CanActivateFn, Router} from '@angular/router';
 import {inject} from "@angular/core";
 import {AuthService} from '../service/auth.service';
+import { ToastrService } from 'ngx-toastr';
 
 export const connectedGuard: CanActivateFn = (route, state) => {
   const loggedIn = inject(AuthService).isLoggedIn();
   if(!loggedIn){
     inject(Router).navigate(['/login']);
-    console.log("You are not connected");
+    inject(ToastrService).error('You must be connected to access this page');
   }
   return loggedIn;
 };
--- src/app/guards/disconnected.guard.ts
+++ src/app/guards/disconnected.guard.ts
@@ -1,12 +1,13 @@
 import {CanActivateFn, Router} from '@angular/router';
 import {inject} from "@angular/core";
 import {AuthService} from '../service/auth.service';
+import { ToastrService } from 'ngx-toastr';
 
 export const disconnectedGuard: CanActivateFn = (route, state) => {
   const loggedIn = inject(AuthService).isLoggedIn();
   if(loggedIn){
     inject(Router).navigate(['/home']);
-    console.log("You are already connected");
+    inject(ToastrService).error('You are already connected');
   }
   return !loggedIn;
 };
