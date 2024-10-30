713405d2ec4631a09051fac929423ec20eee6a12
julien <chadow.video@gmail.com>
2024-10-30 02:17:11 +0100

    feat : server side rendering + dockerize

--- src/app/interceptors/auth-interceptor.ts
+++ src/app/interceptors/auth-interceptor.ts
@@ -1,20 +1,16 @@
 import {HttpErrorResponse, HttpInterceptorFn} from '@angular/common/http';
-import {inject, PLATFORM_ID} from "@angular/core";
+import {inject} from "@angular/core";
 import {catchError, throwError} from "rxjs";
 import {Router} from "@angular/router";
 import {AuthService} from '../service/auth.service';
 import {ToastrService} from 'ngx-toastr';
-import {isPlatformBrowser} from '@angular/common';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
+
   const authService = inject(AuthService);
   const jwtToken = authService.getCurrentToken();
-  const platformId = inject(PLATFORM_ID);
-
-  // Only inject Router and ToastrService if running in the browser
-  const router = isPlatformBrowser(platformId) ? inject(Router) : null;
-  const toastr = isPlatformBrowser(platformId) ? inject(ToastrService) : null;
-
+  const router = inject(Router);
+  const toastr = inject(ToastrService);
   if (jwtToken) {
     req = req.clone({
       setHeaders: {
@@ -22,22 +18,16 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
       }
     });
   }
-
   return next(req).pipe(
     catchError((error: HttpErrorResponse) => {
       if (error.status === 401) {
         authService.logout();
-
-        if (isPlatformBrowser(platformId) && router && toastr) {
-          router.navigate(['/login']).then(() => toastr.error("You are not authenticated"));
-        }
+        router.navigate(['/login']).then(() =>toastr.error("You are not authenticated"));
       }
       if (error.status === 403) {
-        if (isPlatformBrowser(platformId) && router && toastr) {
-          router.navigate(['/login']).then(() => toastr.error("You are not authorized"));
-        }
+        router.navigate(['/login']).then(() => toastr.error("You are not authorized"));
       }
-      return throwError(() => error);
+      return throwError(() => error)
     })
   );
 };
