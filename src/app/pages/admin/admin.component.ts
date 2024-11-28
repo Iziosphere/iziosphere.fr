53287c3eb21f53c7843b75164be85087014bb77d
julien <julien.sanchez74@orange.fr>
2024-11-28 23:07:42 +0100

    feat : changing user info in panel admin

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,5 +1,8 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import { RouterModule } from '@angular/router';
+import {UserService} from '../../service/user.service';
+import {UserDto} from '../../models/user.model';
+import {AuthService} from '../../service/auth.service';
 
 @Component({
     selector: 'app-admin',
@@ -8,6 +11,21 @@ import { RouterModule } from '@angular/router';
     styleUrl: './admin.component.scss',
     encapsulation: ViewEncapsulation.None // Désactiver l'encapsulation
 })
-export class AdminComponent {
+export class AdminComponent implements OnInit{
+
+    session? : UserDto;
+    constructor(private userService : UserService,private authService: AuthService) {
+    }
+
+    ngOnInit(): void {
+        this.userService.getMyInfos().subscribe((user: UserDto) => {
+            this.session = user;
+          console.log(this.session);
+        });
+    }
+
+    logout() {
+        this.authService.logout();
+    }
 
 }
